a13e885c08430602f056fbffc763989b19135e42
Maleakhiano <paskaaae999@student.untan.ac.id>
2021-02-09 22:45:27 +0700

    praktikum 2 - sintaks dasar php

---
+++ pertemuan2/index.php
@@ -0,0 +1,72 @@
+<?php 
+// ini adalah komentar
+/*
+ini
+juga
+komentar
+*/
+
+// Pertemuan 2 - PHP Dasar
+// SIntaks PHP
+
+// Standar Output
+// echo, print (mencetak tulisan/variabel)
+// print_r (mencetak array)
+// var_dump (mengetahui isi/tipe variabel)
+
+// echo "Maleakhiano Paskarian";
+// print "Maleakhiano Paskarian";
+// print_r("Maleakhiano Paskarian");
+// var_dump("Maleakhiano Paskarian");
+
+// echo true;
+// echo 123;
+
+// Penulisan Sintaks PHP
+// 1. PHP di dalam HTML
+// 2. HTML di dalam PHP
+
+// Variabel dan Tipe data
+// Variabel
+// Variabel tidak boleh diawali dengan angka, tapi boleh mengandung angka
+// $nama = "Paskarian";
+
+// Operator
+// Aritmatika
+// + - * / %
+// $x = 10;
+// $y = 20;
+// echo $x * $y;
+
+// penggabung string / concatenation / concat
+//  operatornya: . (titik)
+// $nama_depan = "Maleakhiano";
+// $nama_belakang = "Paskarian";
+// echo $nama_depan . " " . $nama_belakang;
+
+// Assignment
+// =, +=, -+, *=, /=, %=, .=
+// $x = 1;
+// $x += 5;
+// echo $x;
+// $nama = "Maleakhiano";
+// $nama .= " ";
+// $nama .= "Paskarian";
+// echo $nama;
+
+// Perbandingan (tidak mengecek tipe data)
+//  <, >, <=, >=, ==, !=
+// var_dump(1<5);
+// var_dump(1 == "1"); (bernilai true karena tidak mengecek tipe data)
+
+// Identitas (mengecek tipe data dan nilainya)
+// ===, !==
+// var_dump(1 === "1");
+
+// Logika
+// &&, ||, !
+// $x = 10;
+// var_dump($x > 30 && $x % 2 == 0);
+// var_dump($x > 30 || $x % 2 == 0);
+// ?>
+
